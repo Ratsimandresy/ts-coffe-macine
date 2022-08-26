32a2235b8a349e1098bf84c9328b2a50d7ae0838
ratsimandresy <ricorabesoa@gmail.com>
2022-08-26 17:49:14 +0200

    PriceVerification.spec.ts unit test green

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import {drinksPrices} from "./classes/PriceOfDrink";
-import {DrinkType} from "./classes/DrinkType";
 
-for (let key  of drinksPrices) {
-    console.log(key)
-}
-
-console.log(DrinkType['CHOCOLATE']);
-console.log(DrinkType['TEA']);
 
